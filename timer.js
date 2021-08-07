ccb46263edacf55b41cd9540950c8a4c6de93773
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2021-08-07 12:02:26 -0400

    Fix timer and add pause feature

--- timer.js
+++ timer.js
@@ -1,27 +1,28 @@
+document.getElementById("button").addEventListener("click", pause);
 
-// Set the date we're counting down to
-// var countDownDate = new Date().getTime() + 1500000;
-
-var thingy = true;
-
-if (thingy) {
-    var countDownDate = new Date().getTime() + 10000;
-    document.getElementById("workornot").innerHTML = "Get to work!"
-
-} else {
-    var countDownDate = new Date().getTime() + 10000;
-    document.getElementById("workornot").innerHTML = "Take a break!"
+var paused = false;
+function pause() {
+    if (paused===false){
+        paused = true;
+        document.getElementById("button").innerHTML = "Resume";
+    } else {
+        paused = false;
+        document.getElementById("button").innerHTML = "Pause";
+    }
 }
+
+var work = true;
+const workMinutes=25;
+const breakMinutes=5;
+var distance = workMinutes * 60000;
 
 // Update the count down every 1 second
 var x = setInterval(function() {
+    // Decrease time
+    if (!paused){
+    distance -=1000;
+    }
 
-    // Get today's date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
     // Time calculations for days, hours, minutes and seconds
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -30,10 +31,23 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
     
     // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
+    if (distance < 0) {   
+        work = !work;     
         document.getElementById("demo").innerHTML = "Time's up!";
-        window.alert("Time's up, Take a 5 minute break!", "_blank");
-        thingy = !thingy;
+        
+        if (!work) { // Break is starting
+            distance = breakMinutes * 60000;
+            window.alert("Time's up, Take a "+breakMinutes+" minute break!", "_blank");
+        } else { // Work is starting
+            distance = workMinutes * 60000;
+            window.alert("Time's up, get to work for "+workMinutes+" minutes!", "_blank");
+        }
+    }
+
+    if (work) {
+        document.getElementById("workornot").innerHTML = "Get to work!"
+    
+    } else {
+        document.getElementById("workornot").innerHTML = "Take a break!"
     }
 }, 1000);
